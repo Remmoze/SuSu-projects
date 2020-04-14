eb49e052a91aae6574da480251e5e6d40be56890
Remmoze <remmoze@gmail.com>
2020-04-14 20:42:02 +0500

    stack done

--- Stack/stack/stack.cpp
+++ Stack/stack/stack.cpp
@@ -1,20 +1,34 @@
-// stack.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define MAX_SIZE 100
+class Stack {
+public:
+	void push(int value) {
+		elements[cur++] = value;
+	};
+	void pop() {
+		--cur;
+	};
+	int back() {
+		return elements[cur-1];
+	};
+	int size() {
+		return cur;
+	}
+	void clear() {
+		cur = 0;
+	}
+	void print() {
+		for(int i = 0; i < cur; i++) {
+			std::cout << elements[i];
+		}
+		std::cout << std::endl;
+	}
+private:
+	int cur = 0;
+	int elements[MAX_SIZE];
+};
+
+int main() {
+	Stack stack;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
