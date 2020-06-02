d941175c8bc196c0af56415c77fe38b61bda9ec6
Remmoze <remmoze@gmail.com>
2020-06-02 12:59:44 +0500

    DSU №2787 8/11

--- Olympiad Programming/Disjoint Set Union/№2787 Unite and Rule/№2787 Unite and Rule.cpp
+++ Olympiad Programming/Disjoint Set Union/№2787 Unite and Rule/№2787 Unite and Rule.cpp
@@ -11,8 +11,11 @@ struct Stack {
 	int cur = 0;
 	int max = initialSize;
 	void init();
+	void resizeto(int at_least);
 	void resize();
-	void add(int index, int value);
+	void add(int value);
+	void clear();
+	int find(int value);
 };
 void Stack::init() {
 	p = (int*)malloc(max * sizeof(int));
@@ -20,22 +23,39 @@ void Stack::init() {
 }
 void Stack::resize() {
 	if(cur < max) return;
-	p = (int*)realloc(p, max * growthFactor * sizeof(int));
 	max *= growthFactor;
+	p = (int*)realloc(p, max * sizeof(int));
 	if(p == nullptr) exit(-1);
 }
-void Stack::add(int index, int value) {
-
+void Stack::resizeto(int at_least) {
+	if(at_least < max) return;
+	while(at_least < max) {
+		max *= growthFactor;
+	}
+	p = (int*)realloc(p, max * sizeof(int));
+	if(p == nullptr) exit(-1);
 }
-Stack stack;
+void Stack::add(int value) {
+	resize();
+	p[cur++] = value;
+}
+int Stack::find(int value) {
+	for(int i = 0; i < cur; i++) {
+		if(p[i] == value)
+			return i;
+	}
+	return -1;
+}
+void Stack::clear() {
+	cur = 0;
+}
+Stack query;
+
+int parents[500000] = {};
+
 
 int root;
-int path[500000]; //because why not :D
 int ind = 0;
-
-void add(int v) {
-	path[ind++] = v;
-}
 
 void clear() {
 	ind = 0;
@@ -46,31 +66,43 @@ void make_set(int v) {
 		parents[v] = v;
 }
 
-int traversal(int v) {
-	path[ind++] = v;
-	if(parents[v] == v)
+void traversal(Stack* stack, int v) {
+	stack->add(v);
+	if(parents[v] == v || parents[v] == 0)
 		return;
-	traversal(parents[v]);
+	traversal(stack, parents[v]);
 }
 
-int find_set(int v) {
-	if(parents[v] == v)
-		return v;
-	return find_set(parents[v]);
+int cmp_traversal(Stack* stack, int v) {
+	if(stack->find(v) == -1 && (parents[v] != v && parents[v] != 0))
+		return cmp_traversal(stack, parents[v]);
+	return v;
 }
-
-void union_sets(int a, int b) {
-	parents[b] = a;}
 
 int main() {
-	stack.init();
-	
-
-	int N;
+	query.init();
+	int N, inp1, inp2;
 	std::cin >> N;
 
-	std::string input;
-	for(int i = 1; i < N; i++) {
+	parents[1] = 1;
 
+	std::string input;
+	for(int i = 0; i < N; i++) {
+		std::cin >> input;
+		std::cin >> inp1;
+		std::cin >> inp2;
+		if(input.compare("ADD") == 0) {
+			parents[inp2] = inp1;
+			//printf("%i -> %i\n", inp2, inp1);
+		} else {
+			query.clear();
+			traversal(&query, inp1);
+			std::cout << cmp_traversal(&query, inp2) << std::endl;
+			/*
+			for(int i = 0; i < query.cur; i++) {
+				printf("%i\n", query.p[i]);
+			}
+			*/
+		}
 	}
 }
