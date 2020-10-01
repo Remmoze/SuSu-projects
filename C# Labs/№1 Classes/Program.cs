ebe652a8c9212146ee029ae90f1f919e75896d6d
Remmoze <remmoze@gmail.com>
2020-10-02 02:31:28 +0500

    Improved №1 Classes

--- C# Labs/№1 Classes/Program.cs
+++ C# Labs/№1 Classes/Program.cs
@@ -1,105 +1,32 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Notebook {
-    public enum SearchOptions {
-        name, phone, birthday
+    public class Entry {
+        private DateTime BirthDayDateTime { get; }
+        public string FullName { get; }
+        public string BirthDay {
+            get {
+                return BirthDayDateTime.ToString("dd.MM.yy");
+            }
+        }
+        public string PhoneNumber { get; }
+
+        public Entry(string name, DateTime birthday, string phone) {
+            FullName = name;
+            BirthDayDateTime = birthday;
+            PhoneNumber = phone;
+        }
+
+        public override string ToString() {
+            return $"{FullName} / {PhoneNumber} / {BirthDay}";
+        }
     }
+
     public class Notebook {
-
-
-        public class Entry {
-            public string LastName { get; }
-            public string FirstName { get; }
-            public string MiddleName { get; }
-            public string FullName {
-                get {
-                    return $"{LastName} {FirstName} {MiddleName}";
-                }
-            }
-            private DateTime BirthDayDateTime { get; set; }
-            public string BirthDay {
-                get {
-                    return BirthDayDateTime.ToString("MM.dd.yy");
-                }
-            }
-            public string PhoneNumber { get; }
-
-            public Entry(string[] name, DateTime birthday, string phone) {
-
-                LastName = name[0];
-                FirstName = name[1];
-                MiddleName = name[2];
-
-                BirthDayDateTime = birthday;
-                PhoneNumber = phone;
-            }
-
-            public void PrintFull() {
-                Console.WriteLine($"{FullName} / {PhoneNumber} / {BirthDay}");
-            }
-        }
-
-        private List<Entry> Entries = new List<Entry>();
-
-        public Entry RequestInput() {
-
-            Console.WriteLine("\n* Adding new entry to the list *");
-            Console.WriteLine("Name (ex: LastName FirstName MiddleName):");
-            string[] name = Console.ReadLine().Split(" ");
-            while(name.Length < 3) {
-                Console.WriteLine("Please write name in the correct form.");
-                name = Console.ReadLine().Trim().Split(" ");
-            }
-
-            Console.WriteLine("Date of birth (ex: 23 10 1998):");
-            DateTime date;
-            while(!DateTime.TryParse(Regex.Replace(Console.ReadLine().Trim(), "[^0-9. ]", ""), out date)) { // only allow 0-9 and .
-                Console.WriteLine("Please write the date of birth in the correct form.");
-            }
-            Console.WriteLine("Phone number (ex: +79823214412):");
-            string phone = Regex.Replace(Console.ReadLine().Trim(), "[^0-9+]", ""); // only allow 0-9 and +
-
-            return new Entry(name, date, phone);
-        }
-
-        public Entry RequestRemoval() {
-            Console.WriteLine("* Please write down the index of the desired entry to be removed *");
-            Console.WriteLine("* Write '-1' to exit *");
-            ListEntries();
-
-            string input;
-            int index;
-            while(true) {
-                Console.Write("Index: ");
-                input = Console.ReadLine();
-                if(!int.TryParse(input, out index)) {
-                    Console.WriteLine("Write a correct index value.");
-                    continue;
-                }
-                if(index < -1 || index >= Entries.Count) {
-                    Console.WriteLine("Index is out of bounds.");
-                    continue;
-                }
-                break;
-            }
-
-            if(index == -1)
-                return null;
-            return Entries[index];
-        }
-
-        public void ListEntries() {
-            Console.WriteLine("____________________");
-            for(int i = 0; i < Entries.Count; i++) {
-                Console.Write($"[{i}] ");
-                Entries[i].PrintFull();
-            }
-            Console.WriteLine("____________________");
-        }
+        private readonly List<Entry> Entries = new List<Entry>();
 
         public void AddEntry(Entry entry) {
             Entries.Add(entry);
@@ -109,121 +36,186 @@ namespace Notebook {
             Entries.Remove(entry);
         }
 
-        public void Sort() {
+        public void SortByName() {
             Entries.Sort((x, y) => string.Compare(x.FullName, y.FullName));
         }
 
-        public Entry SearchBy(SearchOptions type) {
+        public IEnumerable<Entry> SearchName(string input) {
+            return Entries.FindAll(x => x.FullName.StartsWith(input));
+        }
 
-            string input;
-            Console.Write("Search input: ");
-            input = Console.ReadLine();
+        public IEnumerable<Entry> SearchPhone(string input) {
+            return Entries.FindAll(x => x.PhoneNumber.Contains(input));
+        }
 
-            switch(type) {
-                case SearchOptions.birthday: {
-                    return Entries.FirstOrDefault(x => x.BirthDay == input);
-                }
-                case SearchOptions.phone: {
-                    return Entries.FirstOrDefault(x => x.PhoneNumber.Contains(input)); // a way to ignore '+' sign
-                }
-                case SearchOptions.name: {
-                    return Entries.FirstOrDefault(x => x.FullName.StartsWith(input)); // can write just portion of the name
-                }
-                default:
-                throw new ArgumentException("Invalid search option");
+        public IEnumerable<Entry> SearchBirthday(string input) {
+            DateTime date;
+            while(!DateTime.TryParse(Regex.Replace(input.Trim(), "[^0-9. ]", ""), out date)) {
+                return null;
             }
+            return Entries.FindAll(x => x.BirthDay == date.ToString("dd.MM.yy"));
+        }
+
+        public List<Entry> GetEntries() {
+            return Entries;
         }
     }
 
     class Program {
         static void Main(string[] args) {
             Notebook book = new Notebook();
-            //test entry
-            book.AddEntry(new Notebook.Entry(new string[] { "Ivanov", "Ivan", "Ivanovich" }, new DateTime(1998, 10, 11), "+9876543210"));
+            book.AddEntry(new Entry("Ivanov Ivan Ivanovich", new DateTime(1998, 10, 11), "+9876543210"));// default entry
 
-            Console.Write("Welcome to Notebook class. ");
+            var options = new string[] { "add", "remove", "sort", "list", "search by name", "search by birthday", "search by phone", "exit"};
             string[] input;
-            PrintOptions();
 
+            Console.Title = "Welcome to Notebook class.";
+            var selected = 0;
             while(true) {
-                input = Console.ReadLine().Split(" ");
-                switch(input[0]) {
-                    case "add": {
-                        var entry = book.RequestInput();
-                        Console.Clear();
-                        book.AddEntry(entry);
-                        Console.WriteLine("Entry successfully added!");
-                        entry.PrintFull();
-                        PrintOptions();
-                        break;
-                    }
-                    case "delete":
-                    case "remove": {
-                        var entry = book.RequestRemoval();
-                        Console.Clear();
-                        if(entry != null) {
-                            book.RemoveEntry(entry);
-                            Console.WriteLine("Entry successfully deleted!");
-                        } else {
-                            Console.WriteLine("Couldn't delete entry!");
-                        }
+                Console.Clear();
 
-                        PrintOptions();
-                        break;
-                    }
-                    case "sort": {
-                        book.Sort();
-
-                        PrintOptions();
-                        break;
-                    }
-                    case "search":
-                    case "find": {
-                        if(input.Length < 2 || !Enum.TryParse(input[1], out SearchOptions option)) {
-                            Console.WriteLine("Incorrect search type");
-                            break;
-                        }
-                        var entry = book.SearchBy(option);
-                        Console.Clear();
-                        if(entry != null)
-                            entry.PrintFull();
-                        PrintOptions();
-                        break;
-                    }
-                    case "list": {
-                        Console.Clear();
-                        book.ListEntries();
-                        PrintOptions();
-                        break;
-                    }
-                    case "exit": {
-                        Console.Clear();
-                        Console.WriteLine("Thank you for using Notebook class!");
-                        Environment.Exit(0);
-                        break;
-                    }
-                    default: {
-                        Console.Clear();
-                        Console.WriteLine("\nPlease write down the correct action from the list given below:");
-                        PrintOptions(false);
-                        break;
-                    }
+                for(int i = 0; i < options.Length; i++) {
+                    if(i == selected)
+                        Console.Write("> ");
+                    Console.WriteLine(options[i]);
                 }
 
+                var command = Console.ReadKey(true).Key;
+                if(command == ConsoleKey.DownArrow)
+                    selected = Math.Min(selected + 1, options.Length-1);
+
+                else if(command == ConsoleKey.UpArrow)
+                    selected = Math.Max(selected - 1, 0);
+
+                else if(command == ConsoleKey.Enter) {
+                    Console.Title = "";
+                    switch(options[selected]) {
+                        case "add": {
+                            book.AddEntry(NewEntry());
+                            Console.Title = "Entry successfully added!";
+                            break;
+                        }
+                        case "remove": {
+                            RemovalSystem(book);
+                            break;
+                        }
+                        case "sort": {
+                            book.SortByName();
+                            Console.Title = "Notebook has been sorted!";
+                            break;
+                        }
+                        case "list": {
+                            Console.Clear();
+                            foreach(var entry in book.GetEntries())
+                                Console.WriteLine(entry);
+                            Console.WriteLine("\nPress any key to continue");
+                            Console.ReadKey(true);
+                            break;
+                        }
+                        case "search by name": {
+                            Console.Clear();
+                            Console.Title = "Search by name: ";
+                            Console.Write("Name: ");
+                            PrintSearch(book.SearchName(Console.ReadLine()));
+                            Console.Title = "";
+                            break;
+                        }
+                        case "search by birthday": {
+                            Console.Clear();
+                            Console.Title = "Search by birthday: ";
+                            Console.Write("Birthday: ");
+                            PrintSearch(book.SearchBirthday(Console.ReadLine()));
+                            Console.Title = "";
+                            break;
+                        }
+                        case "search by phone": {
+                            Console.Clear();
+                            Console.Title = "Search by phone: ";
+                            Console.Write("Phone: ");
+                            PrintSearch(book.SearchPhone(Console.ReadLine()));
+                            Console.Title = "";
+                            break;
+                        }
+                        case "exit": {
+                            Console.Clear();
+                            Console.WriteLine("Thank you for using Notebook class!");
+                            Environment.Exit(0);
+                            break;
+                        }
+                    }
+                    selected = 0;
+                }
             }
         }
 
-        static void PrintOptions(bool showtip = true) {
-            if(showtip) {
-                Console.WriteLine();
-                Console.WriteLine("Please write down the next action: ");
+        public static Entry NewEntry() {
+            Console.Clear();
+            Console.WriteLine("* Adding new entry to the list *");
+            Console.Write("Name: ");
+            string name = Console.ReadLine().Trim();
+
+            Console.Write("Date of birth: ");
+            DateTime date;
+            while(!DateTime.TryParse(Regex.Replace(Console.ReadLine().Trim(), "[^0-9. ]", ""), out date)) { // only allow 0-9 and .
+                Console.WriteLine("Please write the date of birth in the correct form.");
             }
-            Console.WriteLine("* add");
-            Console.WriteLine("* remove");
-            Console.WriteLine("* sort");
-            Console.WriteLine("* find <name/phone/birthday>");
-            Console.WriteLine("* list");
-            Console.WriteLine("* exit");
+
+            Console.Write("Phone number: ");
+            string phone = Console.ReadLine().Trim();
+
+            return new Entry(name, date, phone);
+        }
+
+        public static void RemovalSystem(Notebook book) {
+            Console.Clear();
+            var selected = 0;
+            while(true) {
+                Console.Clear();
+                Console.WriteLine("* Press enter to remove an entry *\n");
+                var entries = book.GetEntries();
+
+                for(int i = 0; i < entries.Count+1; i++) {
+                    if(i == selected)
+                        Console.Write("> ");
+                    if(i < entries.Count)
+                        Console.WriteLine(entries[i]);
+                    else
+                        Console.WriteLine("exit");
+                }
+
+                switch(Console.ReadKey(true).Key) {
+                    case ConsoleKey.DownArrow: {
+                        selected = Math.Min(selected + 1, entries.Count);
+                        break;
+                    }
+                    case ConsoleKey.UpArrow: {
+                        selected = Math.Max(selected - 1, 0);
+                        break;
+                    }
+                    case ConsoleKey.Enter: {
+                        if(selected == entries.Count)
+                            return; //exit selected
+                        book.RemoveEntry(entries[selected]);
+                        selected = 0;
+                        break;
+                    }
+                    
+                }
+            }
+        }
+
+        public static void PrintSearch(IEnumerable<Entry> result) {
+            Console.Clear();
+            if(result == null || result.Count() == 0)
+                Console.WriteLine("Your search didn't return any matches.");
+            else { 
+                Console.WriteLine("Your search returned matches: ");
+                foreach(var entry in result) {
+                    Console.WriteLine(entry);
+                }
+            }
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey(true);
         }
     }
 }
---
+++ C# Labs/№3 Inheritance/Program.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Inheritance {
+
+    public enum PlaneType {
+        Passenger,
+        Cargo
+    }
+
+    public abstract class Plane {
+        public PlaneType type;
+        public string name;
+        public int weight; //tons
+        public abstract int GetFlightLoad(); //kg
+        public Plane(string _name, int _weight) {
+            weight = _weight;
+            name = _name;
+        }
+    }
+
+    public class CargoPlane : Plane {
+        public int cargoWeight;
+        public CargoPlane(string name, int _weight, int cargo) : base(name, _weight) {
+            type = PlaneType.Cargo;
+            cargoWeight = cargo;
+        }
+        public override int GetFlightLoad() {
+            return cargoWeight + weight*1000;
+        }
+    }
+
+    public class PassengerPlane : Plane {
+        public int passengers;
+        public PassengerPlane(string name, int _weight, int _passengers) : base(name, _weight) {
+            passengers = _passengers;
+            type = PlaneType.Passenger;
+        }
+        public override int GetFlightLoad() {
+            return passengers * 62 + weight * 1000;
+        }
+    }
+
+    //public class Boeing_747 : PassangerPlane {
+    //    public Boeing_747() : base(333, 366) {}
+    //}
+    //public class Airbus_A330 : PassangerPlane {
+    //    public Airbus_A330() : base(500, 277) { }
+    //}
+
+    public class Airline {
+        public List<Plane> planes = new List<Plane>();
+        public Airline() {
+            //planes.Add(new PassengerPlane("Boeing_747", 333, 366));
+            //planes.Add(new PassengerPlane("Airbus_A330", 500, 277));
+            //planes.Add(new CargoPlane("TestCargo", 666, 333));
+        }
+
+        public void ImportJSON() {
+            var json = File.ReadAllText("./test.json");
+            planes = JsonConvert.DeserializeObject<List<Plane>>(json);
+        }
+
+        public void ExportJSON() {
+            var json = JsonConvert.SerializeObject(planes);
+            File.WriteAllText("./test.json", json);
+        }
+
+        public void Print() {
+            foreach(var plane in planes) {
+                Console.WriteLine($"{plane.name}, {plane.type}, {plane.GetFlightLoad()}");
+            }
+        }
+    }
+
+    class Program {
+        public static Airline airline = new Airline();
+        static void Main(string[] args) {
+            airline.ImportJSON();
+            airline.Print();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
