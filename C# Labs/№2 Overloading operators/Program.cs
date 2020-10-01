ae9e31f673f5888778ffb092fd0be8e5fcb40ccd
Remmoze <remmoze@gmail.com>
2020-10-02 02:53:46 +0500

    Improved №2 Classes
    Removed arrow oprators, added checks for invalid values.

--- C# Labs/№2 Overloading operators/Program.cs
+++ C# Labs/№2 Overloading operators/Program.cs
@@ -2,37 +2,80 @@
 
 namespace Vectormath {
     public class Vector {
-        public double Length { get; set; }
-        public double Angle { get; set; }
+        private double _Length;
+        public double Length {
+            get { return _Length; }
+            set { _Length = Math.Max(0d, value); }
+        }
+        private double _Angle;
+        public double Angle {
+            get { return _Angle; }
+            set { _Angle = value % 360; }
+        }
 
-        public double Rads => Angle * Math.PI / 180;
-        public double X => Length * Math.Cos(Rads);
-        public double Y => Length * Math.Sin(Rads);
+        public double Rads {
+            get { return Angle * Math.PI / 180; }
+        }
+        public double X {
+            get { return Length * Math.Cos(Rads); }
+        }
+        public double Y {
+            get { return Length * Math.Sin(Rads); }
+        }
 
-        public Vector(double length, double angle) => (Length, Angle) = (length, angle);
+        public Vector(double length, double angle) {
+            (Length, Angle) = (length, angle);
+        }
 
-        private static Vector FromPoint(double x, double y)
-            => new Vector(Math.Sqrt(x * x + y * y), Math.Atan(y / x) * (180 / Math.PI));
+        private static Vector FromPoint(double x, double y) {
+            return new Vector(Math.Sqrt(x * x + y * y), Math.Atan(y / x) * (180 / Math.PI));
+        }
 
-        public static Vector operator +(Vector a, Vector b) => FromPoint(a.X + b.X, a.Y + b.Y);
-        public static Vector operator -(Vector a, Vector b) => FromPoint(a.X - b.X, a.Y - b.Y);
-        public static double operator *(Vector a, Vector b) => a.X * b.X + a.Y * b.Y;
+        public static Vector operator +(Vector a, Vector b) {
+            return FromPoint(a.X + b.X, a.Y + b.Y);
+        }
+        public static Vector operator -(Vector a, Vector b) {
+            return FromPoint(a.X - b.X, a.Y - b.Y);
+        }
+        public static double operator *(Vector a, Vector b) {
+            return a.X * b.X + a.Y * b.Y;
+        }
 
-        public override string ToString() => $"Length: {Length:0.###} | Angle: {Angle:0.###}";
+        public override string ToString() {
+            return $"Length: {Length:0.###} | Angle: {Angle:0.###}";
+        }
     }
 
     class Program {
         static void Main(string[] args) {
-            var rnd = new Random();
+
+            double length = 0;
+            double angle = 0;
+
             while(true) {
                 Console.Clear();
-                var a = new Vector(rnd.Next(1, 10), rnd.Next(360));
-                var b = new Vector(rnd.Next(1, 10), rnd.Next(360));
-                Console.WriteLine($"First Vector: \t{a}");
-                Console.WriteLine($"Second Vector:\t{b}");
-                Console.WriteLine($"Sum:\t\t{a + b}");
-                Console.WriteLine($"Subtration:\t{a - b}");
-                Console.WriteLine($"Dot product:\t{a * b:0.###}");
+
+                Console.WriteLine("First vector:");
+                Console.Write("Length: ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Angle: ");
+                angle = Convert.ToDouble(Console.ReadLine());
+                var VectorA = new Vector(length, angle);
+
+                Console.WriteLine("\nSecond vector:");
+                Console.Write("Length: ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Angle: ");
+                angle = Convert.ToDouble(Console.ReadLine());
+                var VectorB = new Vector(length, angle);
+
+                Console.Clear();
+
+                Console.WriteLine("First Vector: " + VectorA);
+                Console.WriteLine("Second Vector: " + VectorB);
+                Console.WriteLine("Sum: " + (VectorA + VectorB));
+                Console.WriteLine("Subtration: " + (VectorA - VectorB));
+                Console.WriteLine("Dot product: " + (VectorA * VectorB).ToString("0.###"));
 
                 Console.Write("\nContinue? Y/N: ");
                 if(Console.ReadLine() == "N")
